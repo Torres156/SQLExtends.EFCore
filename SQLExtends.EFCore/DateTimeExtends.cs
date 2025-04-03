c911df5c6ac470dd259575399e4c61d10a58c8aa
Gustavo Torres <gts_br@yahoo.com.br>
2025-04-03 15:01:20 -0300

    fix: TimeZone

--- SQLExtends.EFCore/DateTimeExtends.cs
+++ SQLExtends.EFCore/DateTimeExtends.cs
@@ -2,7 +2,7 @@
 
 public static class DateTimeExtends
 {
-    public static TimeZoneInfo TimeZoneDefault { get; set; } = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+    public static TimeZoneInfo TimeZoneDefault { get; set; } = OperatingSystem.IsWindows() ? TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time") : TimeZoneInfo.FindSystemTimeZoneById("America/Sao_Paulo");
 
     public static DateTime ToTimeZone(this DateTime dateTime)
     {
